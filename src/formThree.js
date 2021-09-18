8b56f391796d5ea5ae85ca0c5d3b1ea5e317ca36
anythingultimate <anythingultimate@gmail.com>
2021-09-18 23:08:28 +0530

    Used Formik Custom component

--- src/formThree.js
+++ src/formThree.js
@@ -1,24 +1,58 @@
 import React from 'react';
+import { Formik , Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 const FormThree = () => {
+    const formikProps = {
+        initialValues: { firstname: '' },
+		validationSchema: Yup.object({
+			firstname: Yup
+            .string()
+            .required('Sorry, first name is required')
+		}),
+		onSubmit: (values) => {
+			console.log(values);
+		}
+    }
     return(
-        <div className="container">
-            <div className="col-md-12 mt-5">
-                <form>
-                    <label htmlFor="firstname">First name</label>
-                    <input 
-                        className="form-control" 
-                        type="text" 
-                        name="firstname"
-                    />
-                    
-                    <hr className="mb-4" />
-                    <button className="btn btn-primary btn-lg btn-block" type="submit">
-                        Submit
-                    </button>
-                </form>
-            </div>
-        </div>
+        <Formik {...formikProps}>
+            { formik => (
+                <div className="container">
+                    <div className="col-md-12 mt-5">
+                        <Form>
+                            <div>
+                                <label htmlFor="firstname">First name</label>
+                                <Field name="firstname" type="text" className="form-control mt-1"/>
+                                {formik.values.firstname}
+                                <span className="text-danger"><ErrorMessage name="firstname"/></span>
+                            </div>
+
+                            <div>
+                            <label htmlFor="color" className="mt-4">Color</label>
+                            <Field
+                                as="select"
+                                name="color"
+                                className="form-select mt-1"
+                            >
+                                <option value="">
+                                    Open this to select the Color
+                                </option>
+                                <option value="red">Red</option>
+                                <option value="green">Green</option>
+                                <option value="blue">Blue</option>
+                            </Field>
+                            </div>
+
+                            
+                            <hr className="mb-4" />
+                            <button className="btn btn-primary btn-lg btn-block" type="submit">
+                                Submit
+                            </button>
+                        </Form>
+                    </div>
+                </div>
+            ) }
+        </Formik>
         
     )
 }
